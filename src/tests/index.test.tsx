a3c1063a90e7376ee8d197f4a9fa645db167330d
Eduardo Duran <eduardo.durandiaz@gmail.com>
2023-12-02 23:44:52 -0500

    Clean up old template files

--- src/tests/index.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
-
-it('renders the app', () => {
-  render(<Home />)
-  expect(screen.getByText('Hello Next.js')).toBeInTheDocument()
-})
