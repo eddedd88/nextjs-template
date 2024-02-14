bf81ae3f1e90bc676ff73c0ad0150606f42ba001
Eduardo Duran <eduardo.durandiaz@gmail.com>
2024-02-13 23:55:34 -0500

    Remove prettier default values and unused tailwindcss config file

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  bracketSpacing: true,
   semi: false,
-  trailingComma: 'all',
-  printWidth: 80,
-  tabWidth: 2,
   singleQuote: true,
   jsxSingleQuote: true,
   arrowParens: 'avoid',
