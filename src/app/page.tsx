1b9bc8494c18416b065d66d5a5230ef80faea127
Eduardo Duran <eduardo.durandiaz@gmail.com>
2024-12-19 17:05:33 -0500

    Rename app and components directories to src and update paths in configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between'>
       <p className='mt-8 flex rounded-xl border p-5'>
         Get started at&nbsp;
-        <code className='font-mono font-bold'>/app/page.tsx</code>
+        <code className='font-mono font-bold'>/src/app/page.tsx</code>
       </p>
     </main>
   )
--- src/components/page-not-found.tsx
+++ src/components/page-not-found.tsx
@@ -14,7 +14,8 @@ export default function PageNotFound(props: Props) {
           404 - Page Not Found
         </h1>
         <p className='text-muted-foreground sm:text-lg'>
-          Oops! The page you're looking for doesn't exist or has been moved.
+          Oops! The page you&apos;re looking for doesn&apos;t exist or has been
+          moved.
         </p>
 
         <Button asChild>
