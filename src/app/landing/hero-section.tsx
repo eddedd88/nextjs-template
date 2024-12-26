eeddb18452f16f579f264785a5c1a66deae960c1
Eduardo Duran <eduardo.durandiaz@gmail.com>
2024-12-25 21:06:33 -0500

    Update import statement for JoinWaitlistButton

--- src/app/landing/hero-section.tsx
+++ src/app/landing/hero-section.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
-import JoinWaitlistButton from './join-waitlist-button'
+import { JoinWaitlistButton } from './join-waitlist-button'
 
 export function HeroSection() {
   return (
