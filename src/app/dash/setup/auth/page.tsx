a5c651c84c82ee58c10be98e41f4beb74259edae
Eduardo Duran <eduardo.durandiaz@gmail.com>
2025-01-05 23:50:42 -0500

    Refactor Setup Authentication page to use Typography component for improved readability and add Typography component for consistent text styling

--- src/app/dash/setup/auth/page.tsx
+++ src/app/dash/setup/auth/page.tsx
@@ -1,29 +1,25 @@
 import { Page, PageBackButton, PageTitle } from '@/components/page'
+import { Typography } from '@/components/ui/typography'
 
 export default function SetupAuthPage() {
   return (
     <Page>
       <PageBackButton />
       <PageTitle>Setup Authentication with Auth.js</PageTitle>
-      <ol className='ml-5 list-decimal space-y-6'>
-        <li>Install Auth.js</li>
-        <li>
-          Run in your terminal
-          <div className='mt-2 max-w-52 rounded-md bg-gray-100 p-2'>
-            npx auth secret
-          </div>
-        </li>
-        <li>
-          See the guide at{' '}
-          <a
-            href='https://authjs.dev/getting-started/installation#setup-environment'
-            target='_blank'
-            className='underline hover:text-gray-400'
-          >
-            authjs.dev
-          </a>
-        </li>
-      </ol>
+      <Typography>Run in your terminal:</Typography>
+      <Typography variant='codeBlock' className='mt-2'>
+        npx auth secret
+      </Typography>
+      <Typography>
+        See the guide at{' '}
+        <a
+          href='https://authjs.dev/getting-started/installation#setup-environment'
+          target='_blank'
+          className='underline hover:text-gray-400'
+        >
+          authjs.dev
+        </a>
+      </Typography>
     </Page>
   )
 }
---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
+import { cn } from '@/lib/utils'
+
+const typographyVariants = cva('', {
+  variants: {
+    variant: {
+      p: 'leading-7 [&:not(:first-child)]:mt-6',
+      codeBlock: 'rounded bg-muted p-3 font-mono text-sm block',
+    },
+  },
+  defaultVariants: {
+    variant: 'p',
+  },
+})
+
+export interface TypographyProps
+  extends React.HTMLAttributes<HTMLParagraphElement | HTMLElement>,
+    VariantProps<typeof typographyVariants> {}
+
+export const Typography = ({
+  className,
+  variant,
+  children,
+}: TypographyProps) => {
+  let Comp: React.ElementType = 'p'
+  if (variant === 'codeBlock') {
+    Comp = 'code'
+  }
+  return (
+    <Comp className={cn(typographyVariants({ variant, className }))}>
+      {children}
+    </Comp>
+  )
+}
