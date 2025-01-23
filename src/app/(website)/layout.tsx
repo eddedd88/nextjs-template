7e1b84cd127066c5d72fcee30692e1723c9b1f24
Eduardo Duran <eduardo.durandiaz@gmail.com>
2025-01-22 21:10:13 -0500

    Update gradient background in LayoutLandingPage for improved visual design

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -7,7 +7,7 @@ export default function LayoutLandingPage({
   children: React.ReactNode
 }) {
   return (
-    <div className='flex min-h-screen flex-col bg-gradient-to-b from-white from-10% via-primary/10 to-white to-35% sm:to-50%'>
+    <div className='flex min-h-screen flex-col bg-gradient-to-b from-white from-10% via-primary/10 via-15% to-white to-35%'>
       <header className='container'>
         <NavBar />
       </header>
