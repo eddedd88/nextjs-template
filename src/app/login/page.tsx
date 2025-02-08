b510908b8a1544b80d5360cf39a1ef1168408b2d
Eduardo Duran <eduardo.durandiaz@gmail.com>
2025-02-07 23:42:24 -0500

    Refactor login page and form for improved layout and social login options

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,18 +6,13 @@ import { COMPANY_NAME } from '@/constants'
 export default function LoginPage() {
   return (
     <div className='flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10'>
-      <div className='flex w-full max-w-sm flex-col gap-6'>
-        <Link
-          href='/'
-          className='flex items-center gap-2 self-center font-medium'
-        >
-          <div className='flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground'>
-            <GalleryVerticalEnd className='size-4' />
-          </div>
-          {COMPANY_NAME}
-        </Link>
-        <LoginForm />
-      </div>
+      <Link href='/' className='flex items-center gap-3 text-xl'>
+        <div className='flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground'>
+          <GalleryVerticalEnd className='size-4' />
+        </div>
+        Log in to {COMPANY_NAME}
+      </Link>
+      <LoginForm />
     </div>
   )
 }
