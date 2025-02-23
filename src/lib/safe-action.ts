e711d9b17a46496d8c458cd43a5c30850ceea70f
Eduardo Duran <eduardo.durandiaz@gmail.com>
2025-02-23 13:31:29 -0500

    Add custom error class for improved error handling in safe actions

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -2,10 +2,19 @@ import { createSafeActionClient } from 'next-safe-action'
 import { auth } from './auth'
 import { UNEXPECTED_ERROR_MESSAGE } from '../constants'
 
+/**
+ * Custom error class to be used in actions to
+ * display a custom error message to the user
+ */
+export class ActionError extends Error {}
+
 export const safeAction = createSafeActionClient({
   defaultValidationErrorsShape: 'flattened',
   handleServerError(e) {
     console.error(e)
+    if (e instanceof ActionError) {
+      return e.message
+    }
     return UNEXPECTED_ERROR_MESSAGE
   },
 }).use(async ({ next, ctx }) => {
