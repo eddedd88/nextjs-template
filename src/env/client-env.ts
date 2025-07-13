ffd7587ae511e5ad804167533ec0fc1cf6c0158b
Eduardo Duran <eduardo.durandiaz@gmail.com>
2025-07-13 01:25:26 -0400

    Enhance client environment variable validation: update comments for clarity and add error handling for invalid keys

--- src/env/client-env.ts
+++ src/env/client-env.ts
@@ -3,7 +3,7 @@ import { throwMissingEnvError } from './throw-missing-env-error'
 
 const EnvSchema = z.object({
   // Define your client-side environment variables here,
-  // they should be prefixed with NEXT_PUBLIC_, for example:
+  // they must be prefixed with NEXT_PUBLIC_, for example:
   // NEXT_PUBLIC_API_KEY: z.string().min(1),
 })
 
@@ -11,6 +11,16 @@ const parsedEnv = EnvSchema.safeParse({
   // populate the environment variables from process.env, for example:
   // NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
 })
+
+const invalidEnvKeys = Object.keys(EnvSchema.keyof().enum).filter(
+  key => !key.startsWith('NEXT_PUBLIC_'),
+)
+if (invalidEnvKeys.length > 0) {
+  throw new Error(
+    'All client-side environment variables must be prefixed with "NEXT_PUBLIC_"' +
+      `\nInvalid environments variables found:\n  ${invalidEnvKeys.join('\n  ')}\n`,
+  )
+}
 
 if (parsedEnv.error) {
   throwMissingEnvError(parsedEnv.error)
