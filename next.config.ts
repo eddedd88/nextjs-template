58b99ad62fd4ee620002f076a667bc9b7c34b027
Eduardo Duran <eduardo.durandiaz@gmail.com>
2025-08-21 23:49:28 -0400

    Update next.config.ts to enable typedRoutes option

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 import '@/env/client-env'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typedRoutes: true,
 }
 
 export default nextConfig
