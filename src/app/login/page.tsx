1a92a996a414521db3e9e6953c88974d986573fd
Eduardo Duran <eduardo.durandiaz@gmail.com>
2026-04-02 23:39:45 -0400

    Wrap Logo component in a Link for improved navigation on LoginPage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,9 @@ import { Logo } from '@/components/logo'
 export default function LoginPage() {
   return (
     <div className='flex min-h-svh flex-col items-center justify-center gap-4 p-6 md:p-10'>
-      <Logo className='mb-4' />
+      <Link href='/'>
+        <Logo className='mb-4' />
+      </Link>
       <Link href='/' className='text-lg'>
         <span>Log in to {COMPANY_NAME}</span>
       </Link>
