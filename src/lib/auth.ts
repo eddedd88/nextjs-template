a9ab9a9bae0b120b116b9b529c49afa6919b2ffb
Eduardo Duran <eduardo.durandiaz@gmail.com>
2026-04-04 00:25:57 -0400

    Refactor getAuthToken function to prepare for future authentication implementation

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,7 @@
+import 'server-only'
+// import { auth, currentUser } from '@clerk/nextjs/server'
+
 export async function getAuthToken() {
   throw new Error('Not implemented')
+  // return (await (await auth()).getToken({ template: 'convex' })) ?? undefined
 }
