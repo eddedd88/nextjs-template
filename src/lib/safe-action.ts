85443a274c2f6edea1d6578480fb67694d1696c1
Eduardo Duran <eduardo.durandiaz@gmail.com>
2026-04-11 22:16:48 -0400

    Update safe-action to use a mock user ID for session handling

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,11 +1,16 @@
 import { createSafeActionClient } from 'next-safe-action'
 import { UNEXPECTED_ERROR_MESSAGE } from '../constants'
 import { PublicError } from './public-error'
+// import { auth } from '@clerk/nextjs/server'
+// import { ConvexError } from 'convex/values'
 
 export const safeAction = createSafeActionClient({
   defaultValidationErrorsShape: 'flattened',
   handleServerError(e) {
     console.error(e)
+    // if (e instanceof ConvexError) {
+    //   return e.data
+    // }
     if (e instanceof PublicError) {
       return e.message
     }
@@ -13,7 +18,8 @@ export const safeAction = createSafeActionClient({
   },
 }).use(async ({ next }) => {
   console.log('Not yet implemented auth check, skipping...')
-  const session = { userId: 'user-123' }
+  const session = { userId: 'fake-user-id' }
+  // const session = await auth()
   if (!session.userId) {
     throw new Error('Session is not valid')
   }
