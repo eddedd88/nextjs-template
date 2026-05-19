c17399feb2428eea336b862e9d54632e5ef229ae
Eduardo Duran <eduardo.durandiaz@gmail.com>
2026-05-18 23:47:43 -0400

    feat: add SSO callback page for Clerk.js integration

---
+++ src/app/(dashboard)/login/sso-callback/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+// import { AuthenticateWithRedirectCallback } from '@clerk/nextjs'
+
+/**
+ * This page is used by Clerkjs to handle SSO callback.
+ */
+export default function LoginSSOCallbackPage() {
+  return null
+  // return <AuthenticateWithRedirectCallback />
+}
